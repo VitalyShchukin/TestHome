cd6107e3de77b1c34e3926370f04a14ca867b84a
VitalyShchukin <shchukin_v_v@mail.ru>
2022-05-03 16:12:01 +0300

    11

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,8 +6,12 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class AppTest {
 
@@ -17,6 +21,7 @@ public class AppTest {
     public static void startBr() {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterClass
@@ -26,7 +31,13 @@ public class AppTest {
     }
 
     @Test
-    public void navigationBr() {
+    public void navigationBr() throws InterruptedException {
         driver.get("https://www.google.com");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.cssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf" +
+                        " > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"))
+                .sendKeys("qwerty"+Keys.ENTER);
     }
 }
